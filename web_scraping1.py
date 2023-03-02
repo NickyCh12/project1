df8470e363299227773e155dcf5f82c2174c4cb3
NickyCh12 <126681772+NickyCh12@users.noreply.github.com>
2023-03-02 11:48:23 +0800

    Update web_scraping1.py

--- web_scraping1.py
+++ web_scraping1.py
@@ -6,13 +6,13 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 import time
 
-# Define variables and get the website content using selenium
+# Define variables and get the website content using "Selenium"
 i = 1
 w = ExcelWriter('Excel.xlsx',mode='a',engine='openpyxl', if_sheet_exists='overlay')
 browser = webdriver.Chrome() # open a browser
 browser.get("html_link")
 
-# Click the specific icon to nevagate to the page we want. Sleep for 5 seconds for loading
+# Click the specific icon to nevagate to the page we want // Sleep for 5 seconds for page loading
 element1 = browser.find_element(By.CLASS_NAME,'right-icon')
 element1.click()
 time.sleep(5)
@@ -34,7 +34,7 @@ while i <= 3:
     html = browser.page_source
     soup = BeautifulSoup(html, 'html.parser')
 
-# Get the data we want and assign it into specified column under the dataframe. Input "N/A" if value is not found
+# Get the data we want and assign it into specified column under the dataframe // Input "N/A" if value is not found
     for datetag in soup.select(".deal-property.list .area-block.hidden-sm-and-down"):
         date = datetag.find('div','data-info')
         data['date'].append(date.get_text().strip() if date else 'N/A')
@@ -62,13 +62,13 @@ while i <= 3:
 # Assign the dataframe into table using "Panda" 
     rental = pd.DataFrame(data)   
 
-# Write the table into a pre-created Excel file. Write on the last row on the sheet every time.
+# Write the table into a pre-created Excel file // Write on the last row on the sheet every time
     rental.to_excel(w, startrow=w.sheets['Sheet1'].max_row,index=False, header=False)     
 
-# Find the "Next Page" button on the page and click. Sleep 3 seconds for page loading      
+# Find the "Next Page" button on the page and click // Sleep 3 seconds for page loading      
     element2 = browser.find_element(By.CLASS_NAME,'btn-next')
     element2.click()
-    time.sleep(3) # sleep three seconds so page can load
+    time.sleep(3)
 
 # Save the file
-w.save()     
+w.save()
